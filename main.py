6af72ec6af6366a9c9be2221800d6a6fa00f9280
Srikanth Doddi <srikanth.cloudarch@gmail.com>
2025-11-02 20:49:22 +0530

    feat: file upload functionality Added

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# Write your code here
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+import os
 
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/greet/{name}")
-def read_item(name: str):
-    return {"Hello": name}
+@app.get("/")
+def root():
+    return "Hello RAG fellow!"
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile):
+    folder = "sources"
+    os.makedirs(folder, exist_ok=True)  # Create the folder if it doesn't exist
+
+    file_location = os.path.join(folder, file.filename)
+    with open(file_location, "wb+") as file_object:
+        file_object.write(await file.read())  # Write the file to the directory
+
+    return {"info": "File saved", "filename": file.filename}
